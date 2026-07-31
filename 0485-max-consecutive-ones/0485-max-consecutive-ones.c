6b2fa656d01e333fd412b8d10bb7a8e712c6f3cd
bollamanaswi <bollamanaswi@gmail.com>
2026-07-31 15:15:48 +0530

    Time: 0 ms (100%), Space: 13.3 MB (63.81%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.c
@@ -0,0 +1,19 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+     int count=0;
+     int maxcount=0;
+    for(int i=0;i<numsSize;i++){
+    if (nums[i]==1){
+    count++;
+        }
+    else{
+    if(count>maxcount){
+        maxcount=count;
+    }
+    count=0;
+    }
+    }
+    if(count>maxcount){
+        maxcount=count;
+    }
+    return maxcount;
+}
