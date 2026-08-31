9541fdf3b93b01a78368cc9f2fc72595ab68286d
bollamanaswi <bollamanaswi@gmail.com>
2026-08-31 21:58:35 +0530

    Time: 5 ms (17.52%), Space: 16.9 MB (46.04%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.c
+++ 0896-monotonic-array/0896-monotonic-array.c
@@ -1,13 +1,15 @@
 bool isMonotonic(int* nums, int numsSize) {
-    int increasing =true;
-    int decreasing =true;
+    int increasing=true;
+    int decreasing=true;
     for(int i=0;i<numsSize-1;i++){
         if(nums[i]>nums[i+1]){
             increasing=false;
         }
+    }
+    for(int i=0;i<numsSize-1;i++){
         if(nums[i]<nums[i+1]){
             decreasing=false;
         }
     }
-    return increasing ||decreasing;
+    return increasing || decreasing;
 }
