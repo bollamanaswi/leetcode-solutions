127b1d2d47cbab2bce1f06cca972f4a7bdf12226
bollamanaswi <bollamanaswi@gmail.com>
2026-09-07 18:45:55 +0530

    Time: 0 ms (100%), Space: 9.5 MB (9.88%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.c
@@ -0,0 +1,17 @@
+int findSpecialInteger(int* arr, int arrSize) {
+    int count=1;
+    for(int i=0;i<arrSize-1;i++){
+        if(arr[i]==arr[i+1]){
+            count++;
+        }
+        else{
+            count=1;
+        }
+    if (count*4>arrSize){
+     return arr[i];
+    }
+    }
+    return arr[arrSize - 1];
+
+    
+}
