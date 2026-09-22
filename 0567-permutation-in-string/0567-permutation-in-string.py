3b8a1f05590a046f83d4c34aa6d12d5fa5e4f68e
bollamanaswi <bollamanaswi@gmail.com>
2026-09-22 16:00:58 +0530

    Time: 19 ms (90.89%), Space: 12.8 MB (49.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,43 +1,43 @@
 class Solution(object):
-
     def checkInclusion(self, s1, s2):
         """
         :type s1: str
         :type s2: str
         :rtype: bool
         """
-
-        if len(s1) > len(s2):
+        h1={}
+        h2={}
+        if len(s1)>len(s2):
             return False
-
-        count1 = [0] * 26
-        count2 = [0] * 26
-
-        # Count characters of s1
         for ch in s1:
-            count1[ord(ch) - ord('a')] += 1
-
-        # First window of s2
+            if ch in h1:
+                h1[ch]+=1
+            else:
+                h1[ch]=1
         for i in range(len(s1)):
-            count2[ord(s2[i]) - ord('a')] += 1
-
-        # Check the first window
-        if count1 == count2:
+            ch=s2[i]
+            if ch in h2:
+                h2[ch]+=1
+            else:
+                h2[ch]=1
+        if h1==h2:
             return True
+        for i in range(len(s1),len(s2)):
+            ch=s2[i]
+            if ch in h2:
+                h2[ch]+=1
+            else:
+                h2[ch]=1
+            old=s2[i-len(s1)] 
+            h2[old] -= 1
 
-        # Slide the window
-        for i in range(len(s1), len(s2)):
+            if h2[old] == 0:
+                del h2[old]
 
-            # Add new character
-            count2[ord(s2[i]) - ord('a')] += 1
-
-            # Remove old character
-            count2[ord(s2[i - len(s1)]) - ord('a')] -= 1
-
-            # Compare frequencies
-            if count1 == count2:
+            # Compare both hashmaps
+            if h1 == h2:
                 return True
 
         return False
-
+            
         
