4260ed10fd22bd2a76a0ecf050e4155143d86aa0
bollamanaswi <bollamanaswi@gmail.com>
2026-09-23 16:58:10 +0530

    Time: 95 ms (17.69%), Space: 30.5 MB (8.66%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count =Counter(nums)
+        for i in nums:
+            if count[i]>1:
+                return i
+        
